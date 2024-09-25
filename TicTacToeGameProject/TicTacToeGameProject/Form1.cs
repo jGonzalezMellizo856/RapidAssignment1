4d61bfc543c831827742566577c40e3ba335ce9a
jGonzalezMellizo856 <149606249+jGonzalezMellizo856@users.noreply.github.com>
2024-09-25 13:46:35 -0600

    Add files via upload

---
+++ TicTacToeGameProject/TicTacToeGameProject/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToeGameProject
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Player
+    {
+        public string Player1;
+        public string Player2;
+        public Player() 
+        {
+        
+        }
+    }
+
+    public class CheckButton
+    {
+
+    }
+
+    public class CheckWin 
+    { 
+    
+    }
+
+
+
+}
